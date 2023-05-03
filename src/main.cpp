1ef801398f2464fd62449375957af56d32919f33
Nhan-Office <vntran@thinknkreate.com>
2023-05-03 20:29:35 +0800

    add button and slider examples

--- src/main.cpp
+++ src/main.cpp
@@ -10,29 +10,10 @@ TFT_eSPI tft = TFT_eSPI();  // Invoke custom library
 static lv_disp_draw_buf_t draw_buf;
 static lv_color_t buf[ TFT_WIDTH * TFT_HEIGHT / 10 ];
 
-#if LV_USE_LOG != 0
-/* Serial debugging */
-void my_print( lv_log_level_t level, const char * buf )
-{
-    LV_UNUSED(level);
-    Serial.flush();
-}
-#endif
-
-/* Display flushing */
-void my_disp_flush( lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color_p )
-{
-    uint32_t w = ( area->x2 - area->x1 + 1 );
-    uint32_t h = ( area->y2 - area->y1 + 1 );
-
-    tft.startWrite();
-    tft.setAddrWindow( area->x1, area->y1, w, h );
-    tft.pushColors( ( uint16_t * )&color_p->full, w * h, true );
-    tft.endWrite();
-
-    lv_disp_flush_ready( disp );
-}
-
+// Function prototypes
+void my_disp_flush( lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color_p );
+void lv_example_get_started_2(void);
+void lv_example_get_started_4(void);
 
 void setup(void) {
   Serial.begin( 115200 );
@@ -78,8 +59,69 @@ void setup(void) {
   lv_obj_t *label = lv_label_create( lv_scr_act() );
   lv_label_set_text( label, LVGL_Arduino.c_str() );
   lv_obj_align( label, LV_ALIGN_TOP_LEFT, 0, 0 );
+
+  /* Button with label */
+  lv_example_get_started_2();
+
+  /* Slider with label */
+  lv_example_get_started_4();
 }
 
 void loop() {
   lv_timer_handler(); /* let the GUI do its work */
+}
+
+#if LV_USE_LOG != 0
+/* Serial debugging */
+void my_print( lv_log_level_t level, const char * buf )
+{
+    LV_UNUSED(level);
+    Serial.flush();
+}
+#endif
+
+/* Display flushing */
+void my_disp_flush( lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color_p )
+{
+    uint32_t w = ( area->x2 - area->x1 + 1 );
+    uint32_t h = ( area->y2 - area->y1 + 1 );
+
+    tft.startWrite();
+    tft.setAddrWindow( area->x1, area->y1, w, h );
+    tft.pushColors( ( uint16_t * )&color_p->full, w * h, true );
+    tft.endWrite();
+
+    lv_disp_flush_ready( disp );
+}
+
+/**
+ * Create a button with a label and react on click event.
+ */
+void lv_example_get_started_2(void)
+{
+    lv_obj_t * btn = lv_btn_create(lv_scr_act());     /*Add a button the current screen*/
+    lv_obj_set_pos(btn, 10, 30);                            /*Set its position*/
+    lv_obj_set_size(btn, 120, 50);                          /*Set its size*/
+    // lv_obj_add_event(btn, btn_event_cb, LV_EVENT_ALL, NULL);           /*Assign a callback to the button*/
+
+    lv_obj_t * label = lv_label_create(btn);          /*Add a label to the button*/
+    lv_label_set_text(label, "Button");                     /*Set the labels text*/
+    lv_obj_center(label);
+}
+
+/**
+ * Create a slider and write its value on a label.
+ */
+void lv_example_get_started_4(void)
+{
+    /*Create a slider in the center of the display*/
+    lv_obj_t * slider = lv_slider_create(lv_scr_act());
+    lv_obj_set_width(slider, 200);                          /*Set the width*/
+    lv_obj_center(slider);                                  /*Align to the center of the parent (screen)*/
+    // lv_obj_add_event(slider, slider_event_cb, LV_EVENT_VALUE_CHANGED, NULL);     /*Assign an event function*/
+
+    /*Create a label above the slider*/
+    static lv_obj_t * label = lv_label_create(lv_scr_act());
+    lv_label_set_text(label, "0");
+    lv_obj_align_to(label, slider, LV_ALIGN_OUT_TOP_MID, 0, -15);    /*Align top of the slider*/
 }
